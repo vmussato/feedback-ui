560618c31ad5b815848f2f14765030fc69412e76
Vinícius Mussato <vinicius.mussato@gmail.com>
2021-12-09 17:10:04 -0300

    using lists

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
 function App() {
+
+  const title = 'Blog Post';
+  const body = 'This is my blog post';
+  const comments = [
+    {id: 1, text: 'Comment one'},
+    {id: 2, text: 'Comment two'},
+    {id: 3, text: 'Comment three'},
+  ]
+
   return (
     <div className='container'>
-      <h1>My App</h1>
+      <h1>{title.toUpperCase()}</h1>
+      <p>{body}</p>
+
+      <div className='comments'>
+        <h3>Comments ({comments.length})</h3>
+        <ul>
+          {comments.map((comment, index) => (
+            <li key={index}>{comment.text}</li>
+          ))}
+        </ul>
+      </div>
     </div>
   )
 }
